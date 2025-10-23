7bc960f6e6bdff55e6ca52f2b7e6c97ebe5d5665
sazemlame <25094541+sazemlame@users.noreply.github.com>
2025-10-23 19:36:11 +0530

    Simple random path robot for parcel delivery

--- robot.js
+++ robot.js
@@ -49,8 +49,51 @@ class VillageState{
 
 
 let first = new VillageState("Post Office",[{place:"Post Officce",
-    address:"Alice's House"}]);
+    address:"Alice's House"}]
+);
 
 let next = first.move("Alice's House");
+
+function runRobot(state,robot,memory){
+
+    for(let turn=0;; turn++){
+        if(state.parcels.length == 0)
+        {
+            console.log(`Done in ${turn} turns`);
+            break;
+        }
+        let action = robot(state, memory);
+        state = state.move(action.direction);
+        memory = action.memory;
+        console.log(`Moved to ${action.direction}`);
+        
+    }
+}
+
+
+function randomPick(array){
+    let choice = Math.floor(Math.random()*array.length);
+    return array[choice];
+}
+
+function randomRobot(state){
+    return {direction: randomPick(roadGraph[state.place])};
+}
+
+VillageState.random = function(parcelCount = 5){
+    let parcels =[];
+    for(let i=0;i<parcelCount;i++){
+        let address= randomPick(Object.keys(roadGraph));
+        let place;
+        do{
+            place = randomPick(Object.keys(roadGraph));
+
+        }while(place==address);
+        parcels.push({place,address});
+    }
+    return new VillageState("Post Office",parcels);
+
+};
+
 
 
