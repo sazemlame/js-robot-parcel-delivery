80c69dec69a35ecc4082cb01ff4b4884c9ec5d55
sazemlame <25094541+sazemlame@users.noreply.github.com>
2025-10-23 20:26:56 +0530

    Route robot module added

--- robot.js
+++ robot.js
@@ -95,5 +95,17 @@ VillageState.random = function(parcelCount = 5){
 
 };
 
+const mailRoute =["Alice's House", "Cabin", "Alice's House", "Bob's House",
+"Town Hall", "Daria's House", "Ernie's House",
+"Grete's House", "Shop", "Grete's House", "Farm",
+"Marketplace", "Post Office"];
 
 
+function routeRobot (state,memory){
+    if(memory.length==0){
+        memory = mailRoute;
+    }
+    return {direction: memory[0],memory: memory.slice(1)}
+}
+
+runRobot(VillageState.random(), routeRobot, []);
